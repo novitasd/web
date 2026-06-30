eefe3df1a55923f2f74951afc5121eacadadaede
sandro <hachahuisandro@gmail.com>
2026-06-30 10:45:08 -0500

    CONTAC

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,9 +1,11 @@
 import ContactHero from "../components/contacto/ContactHero";
+import ContactSection from "../components/contacto/ContactSection";
 
 function Contacto() {
   return (
     <>
     <ContactHero/>
+    <ContactSection/>
     </>
   );
 }
